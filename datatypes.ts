9a3a9f25f4c238fee8cee6ccde9fb33fec0b6fbd
Aditya Khatry <adityak@lims1.com>
2023-08-02 15:21:38 +1000

    First Commit

--- datatypes.ts
+++ datatypes.ts
@@ -9,3 +9,12 @@ age = 28;
 
 let isValid : boolean = true;
 console.log(isValid);
+
+let listA : string[];
+listA = ["A", "B","C"];
+
+let listNum : Array<number>;
+listNum = [1,2,3,4,5];
+let results = listNum.filter((num)=>num>2);
+let num = listNum.find((num)=> num ===2);
+console.log(results);
